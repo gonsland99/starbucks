4479a82be44523eafe1223e25bcfc8d78eb2be86
gon <gonsland99@gmail.com>
2022-02-18 11:16:57 +0900

    'visual-section'

--- js/main.js
+++ js/main.js
@@ -3,13 +3,40 @@ const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
 searchEl.addEventListener('click',function(){
-  searchInputEl.focus()
-})
+  searchInputEl.focus();
+});
 searchInputEl.addEventListener('focus',function(){
-  searchEl.classList.add('focused')
-  searchInputEl.setAttribute('placeholder','통합검색')
-})
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused')
-  searchInputEl.setAttribute('placeholder', '')
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder','통합검색');
+});
+searchInputEl.addEventListener('blur',function(){
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder','');
+});
+
+// badge
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll',_.throttle(function(){
+  if(window.scrollY > 500){
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300));
+
+// fadeIn
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+fadeEls.forEach( function(fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: index * .7,
+    opacity: 1
+  })
 })
